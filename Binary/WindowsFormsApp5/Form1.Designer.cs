e3d6e9817d650c841b2ac537092230a66c499ed6
leslie <lajor965@learner.dvusd.org>
2025-01-24 08:49:02 -0700

    binary finish

--- Binary/WindowsFormsApp5/Form1.Designer.cs
+++ Binary/WindowsFormsApp5/Form1.Designer.cs
@@ -67,7 +67,6 @@
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
-            this.digit1 = new System.Windows.Forms.TextBox();
             this.button11 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -475,16 +474,6 @@
             this.button10.UseVisualStyleBackColor = true;
             this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
-            // digit1
-            // 
-            this.digit1.Font = new System.Drawing.Font("Microsoft Sans Serif", 31.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.digit1.Location = new System.Drawing.Point(479, 314);
-            this.digit1.Name = "digit1";
-            this.digit1.ReadOnly = true;
-            this.digit1.Size = new System.Drawing.Size(80, 67);
-            this.digit1.TabIndex = 39;
-            this.digit1.Text = "0";
-            // 
             // button11
             // 
             this.button11.Location = new System.Drawing.Point(632, 329);
@@ -501,7 +490,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button11);
-            this.Controls.Add(this.digit1);
             this.Controls.Add(this.button10);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
@@ -589,7 +577,6 @@
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.TextBox digit1;
         private System.Windows.Forms.Button button11;
     }
 }
