4083db7190fe6632a1b77ca51c25ed35958f4af1
leslie <lajor965@learner.dvusd.org>
2025-02-12 09:08:31 -0700

    quartet start

---
+++ Quartet/Quartet/CarCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quartet
+{
+    internal class CarCard
+    {
+        public int maxspeed { get; set; }
+        public double zerotosixty { get; set; }
+        public int hp { get; set; }
+        public int cc { get; set; }
+        public int cylinders { get; set; }
+        public int rpm { get; set; }
+    }
+}
