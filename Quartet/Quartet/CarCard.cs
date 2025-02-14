85637e37d554cad7409a5a6e5f7330e9f4ff68ea
leslie <lajor965@learner.dvusd.org>
2025-02-14 08:39:41 -0700

    quartet

--- Quartet/Quartet/CarCard.cs
+++ Quartet/Quartet/CarCard.cs
@@ -8,11 +8,22 @@ namespace Quartet
 {
     internal class CarCard
     {
+        public string name { get; set; }
         public int maxspeed { get; set; }
         public double zerotosixty { get; set; }
         public int hp { get; set; }
         public int cc { get; set; }
         public int cylinders { get; set; }
         public int rpm { get; set; }
+        public CarCard(string name, int maxspeed, double zerotosixty, int hp, int cc, int cylinders, int rpm)
+        {
+            this.name = name;
+            this.maxspeed = maxspeed;
+            this.zerotosixty = zerotosixty;
+            this.hp = hp;
+            this.cc = cc;
+            this.cylinders = cylinders;
+            this.rpm = rpm;
+        }
     }
 }
--- Quartet/Quartet/Program.cs
+++ Quartet/Quartet/Program.cs
@@ -10,6 +10,11 @@ namespace Quartet
     {
         static void Main(string[] args)
         {
+            CarCard C4 = new CarCard("Corvette Coupe", 281, 5.2, 344, 5665, 8, 5400);
+            CarCard C3 = new CarCard("Porsche 911 Targa", 285, 5.2, 320, 3596, 6, 6800);
+            CarCard A3 = new CarCard("Ferrari F430 F1", 315, 4.0, 490, 4308, 8, 8500);
+            CarCard G4 = new CarCard("Porsche Cayman S", 275, 5.4, 295, 3387, 6, 6250);
+            CarCard G1 = new CarCard("Marcos Mantara", 225, 5.4, 190, 3998, 8, 4750);
         }
     }
 }
