0013ed55bfaf1df8711dca8b5e0be50689547801
leslie <lajor965@learner.dvusd.org>
2025-02-19 08:52:08 -0700

    quartet cards all added

--- Quartet/Quartet/CarCard.cs
+++ Quartet/Quartet/CarCard.cs
@@ -8,6 +8,8 @@ namespace Quartet
 {
     internal class CarCard
     {
+        public enum category { maxspeed, zerotosixty, hp, cc,
+                                cylinders, rpm}
         public string name { get; set; }
         public int maxspeed { get; set; }
         public double zerotosixty { get; set; }
@@ -15,8 +17,11 @@ namespace Quartet
         public int cc { get; set; }
         public int cylinders { get; set; }
         public int rpm { get; set; }
-        public CarCard(string name, int maxspeed, double zerotosixty, int hp, int cc, int cylinders, int rpm)
+
+        public string id { get; set; }
+        public CarCard(string id, string name, int maxspeed, double zerotosixty, int hp, int cc, int cylinders, int rpm)
         {
+            this.id = id.ToUpper();
             this.name = name;
             this.maxspeed = maxspeed;
             this.zerotosixty = zerotosixty;
@@ -24,6 +29,56 @@ namespace Quartet
             this.cc = cc;
             this.cylinders = cylinders;
             this.rpm = rpm;
+        }
+        public CarCard Compare(CarCard other, category c)
+        {
+            if (other == null) return this;
+            //maxspeed
+            if (c == category.maxspeed) 
+                if (maxspeed > other.maxspeed)
+                    return this;
+                else
+                    return other;
+            //zerotosixty
+            if (c == category.zerotosixty)
+                if (zerotosixty < other.zerotosixty)
+                    return this;
+                else
+                    return other;
+            //hp
+            if (c == category.hp)
+                if (hp > other.hp)
+                    return this;
+                else
+                    return other;
+            //cc
+            if (c == category.cc)
+                if (cc > other.cc)
+                    return this;
+                else
+                    return other;
+            //cylinders
+            if (c == category.cylinders)
+                if (cylinders > other.cylinders)
+                    return this;
+                else
+                    return other;
+            //rpm
+            if (c == category.rpm)
+                if (rpm > other.rpm)
+                    return this;
+                else
+                    return other;
+            return null;
+        }
+        public override string ToString()
+        {
+            return name + ":\n\tmaxspeed:" + maxspeed +
+                "\n\tzerotosixty:" + zerotosixty +
+                "\n\thp:" + hp +
+                "\n\tcc:" + cc +
+                "\n\tcylinders:" + cylinders +
+                "\n\trpm:" + rpm;
         }
     }
 }
